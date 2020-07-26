e59be9188821d84ae177d89a141abfcbb983cd40
Morgan Seznec <contact@morganseznec.com>
2020-07-26 22:36:22 +0200

    Remove bastion resources

--- output.tf
+++ output.tf
@@ -17,10 +17,3 @@ output "db_private_subnet_id" {
   value = aws_subnet.db_private_subnet.id
   description = "The DB private subnet ID"
 }
-
-output "bastion_public_subnet_id" {
-  value = aws_subnet.bastion_public_subnet.0.id
-  description = "The Bastion public subnet ID"
-
-  depends_on = [ aws_subnet.bastion_public_subnet.0 ]
-}
